2287995cd0685fabce5437e10f9fc57d313697df
thai.phamquoc <110405911+Cut666@users.noreply.github.com>
2025-05-22 17:02:49 +0700

    scheduler poll

---
+++ src/bot/commands/poll/poll-scheduler.service.ts
@@ -0,0 +1,47 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MezonBotMessage } from 'src/bot/models/mezonBotMessage.entity';
+import { IsNull, LessThan, Not, Repository } from 'typeorm';
+import { PollService } from './poll.service';
+// import { Cron, CronExpression } from '@nestjs/schedule';
+import { CronJob } from 'cron';
+
+@Injectable()
+export class PollSchedulerService {
+  constructor(
+    @InjectRepository(MezonBotMessage)
+    private mezonBotMessageRepository: Repository<MezonBotMessage>,
+    private pollService: PollService,
+  ) {
+    this.startCronJobs();
+  }
+
+  startCronJobs(): void {
+      const job = new CronJob(
+        '0 * * * *',
+        () => {
+          this.handleResultPollExpire();
+        },
+        null,
+        true,
+        'Asia/Ho_Chi_Minh',
+      );
+  
+      job.start();
+    }
+
+  async handleResultPollExpire() {
+    const currentTimestamp = Date.now();
+    const sevenDaysInMs = 7 * 24 * 60 * 60 * 1000;
+    const timestampMinus12Hours = new Date(+currentTimestamp - sevenDaysInMs);
+
+    const findMessagePolls = await this.mezonBotMessageRepository.find({
+      where: { createAt: LessThan(+timestampMinus12Hours), deleted: false, pollResult: Not(IsNull()) },
+    });
+    
+    if (!findMessagePolls?.length) return;
+    findMessagePolls.map((findMessagePoll) => {
+      this.pollService.handleResultPoll(findMessagePoll);
+    });
+  }
+}
