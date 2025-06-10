37629c1f54f81423e5ca21fbbe5230c3fff32167
michael.mai <michael.mai.thatera@gmail.com>
2025-06-10 12:02:41 +0700

    fix-slots-bot

--- src/bot/commands/transaction/checktransaction.command.ts
+++ src/bot/commands/transaction/checktransaction.command.ts
@@ -10,6 +10,8 @@ import { EUserError } from 'src/bot/constants/error';
 
 @Command('chk')
 export class ChecktransactionCommand extends CommandMessage {
+  private queue: ChannelMessage[] = [];
+  private running = false;
   constructor(
     clientService: MezonClientService,
     @InjectRepository(Transaction)
@@ -18,9 +20,49 @@ export class ChecktransactionCommand extends CommandMessage {
     private userRepository: Repository<User>,
   ) {
     super(clientService);
+    this.startWorker();
+  }
+
+  private async startWorker() {
+    if (this.running) return;
+    this.running = true;
+    setInterval(async () => {
+      if (this.queue.length === 0) return;
+      const msg = this.queue.shift();
+      if (msg) await this.processCheckTransaction(msg);
+    }, 100);
+  }
+
+  async getTotalAmountUser() {
+    const result = await this.userRepository
+      .createQueryBuilder('user')
+      .select('SUM(user.amount)', 'total_amount')
+      .where('user.amount > 0')
+      .getRawOne();
+
+    const totalAmount = result.total_amount;
+    return totalAmount;
   }
 
   async execute(args: string[], message: ChannelMessage) {
+    (message as any).args = args;
+    if (args[0] === 'admin') {
+      if (message.sender_id !== '1827994776956309504') return;
+      const messageChannel = await this.getChannelMessage(message);
+      const totalAmount = await this.getTotalAmountUser();
+      const findBot = await this.userRepository.findOne({
+        where: { user_id: process.env.UTILITY_BOT_ID },
+      });
+      await messageChannel?.reply({
+        t: `Tổng tiền user: ${totalAmount - (findBot?.amount ?? 0)}, Tiền POT: ${findBot?.jackPot ?? 0}, , Tiền user + pot: ${totalAmount - (findBot?.amount ?? 0) + (findBot?.jackPot ?? 0)} \n , Tiền bot: ${findBot?.amount ?? ''}`,
+      });
+      return;
+    }
+    this.queue.push(message);
+  }
+
+  async processCheckTransaction(message: ChannelMessage) {
+    const args = (message as any).args;
     const messageChannel = await this.getChannelMessage(message);
 
     if (message.username === 'Anonymous' || !args[0]) {
@@ -51,19 +93,18 @@ export class ChecktransactionCommand extends CommandMessage {
       } catch (error) {
         const content = `Lỗi khi check transaction!`;
 
-      return await messageChannel?.reply({
-        t: content,
-        mk: [
-          {
-            type: EMarkdownType.PRE,
-            s: 0,
-            e: content.length,
-          },
-        ],
-      });
+        return await messageChannel?.reply({
+          t: content,
+          mk: [
+            {
+              type: EMarkdownType.PRE,
+              s: 0,
+              e: content.length,
+            },
+          ],
+        });
       }
       const cutoffDate = new Date('2025-06-06T00:00:00.000Z'); // sau ngày 07/06/2025 thì return
-      console.log('transaction', transaction)
       const createdAt = new Date(transaction?.create_time);
       if (!transaction || createdAt < cutoffDate) {
         const content = `[Transaction] transaction không tồn tại hoặc quá ngày kiểm tra`;
@@ -91,12 +132,21 @@ export class ChecktransactionCommand extends CommandMessage {
           createAt: new Date(transaction.create_time).getTime(),
         };
         await this.transactionRepository.insert(trans);
-
-        const findUser = await this.userRepository.findOne({
-          where: { user_id: message.sender_id },
+        const users = await this.userRepository.find({
+          where: [
+            { user_id: message.sender_id },
+            { user_id: process.env.UTILITY_BOT_ID },
+          ],
         });
+        const findUser = users.find(
+          (user) => user.user_id === message.sender_id,
+        );
 
-        if (!findUser) {
+        const botInfo = users.find(
+          (user) => user.user_id === process.env.UTILITY_BOT_ID,
+        );
+
+        if (!findUser || !botInfo) {
           return await messageChannel?.reply({
             t: EUserError.INVALID_USER,
             mk: [
@@ -108,8 +158,20 @@ export class ChecktransactionCommand extends CommandMessage {
             ],
           });
         }
-        const newUserAmount = Number(findUser.amount) + Number(transaction.amount);
-        await this.userRepository.update({user_id: message.sender_id}, {amount: newUserAmount});
+        const newUserAmount =
+          Number(findUser.amount) + Number(transaction.amount);
+        const newBotAmount =
+          Number(botInfo.amount) + Number(transaction.amount);
+        await Promise.all([
+          this.userRepository.update(
+            { user_id: message.sender_id },
+            { amount: newUserAmount },
+          ),
+          this.userRepository.update(
+            { user_id: process.env.UTILITY_BOT_ID },
+            { amount: newBotAmount },
+          ),
+        ]);
 
         const content = `[Transaction] Đã cập nhật lại token`;
         return await messageChannel?.reply({
