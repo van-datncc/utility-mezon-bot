e7af603a0b887fe0cbecdd02bfc38e4a706d6989
van-datncc <dat.dangvan@ncc.asia>
2025-06-13 09:51:14 +0700

    feat: suggest command

--- src/bot/base/command.handle.ts
+++ src/bot/base/command.handle.ts
@@ -1,29 +1,51 @@
-import { ChannelMessage } from 'mezon-sdk';
+import { ChannelMessage, EMarkdownType } from 'mezon-sdk';
 import { Injectable } from '@nestjs/common';
 import { ModuleRef } from '@nestjs/core';
 import { CommandStorage } from '../base/storage';
 import { CommandMessage } from './command.abstract';
 import { extractMessage } from '../utils/helps';
 import { CommandBaseInterface } from './interfaces/asterisk.interface';
+import { generateSuggestionMessage } from '../utils/suggest';
 
 @Injectable()
 export class CommandBase implements CommandBaseInterface {
   public commandList: { [key: string]: CommandMessage };
 
-  constructor(
-    private readonly moduleRef: ModuleRef
-  ) {}
+  constructor(private readonly moduleRef: ModuleRef) {}
 
-  execute(messageContent: string, message: ChannelMessage) {
-    const [commandName, args] = extractMessage(messageContent);
+  execute(
+    messageContent: string,
+    message: ChannelMessage,
+    commandName?: string,
+  ): null[] {
+    const [extractedCommandName, args] = extractMessage(messageContent);
+    const cmdName = commandName || extractedCommandName;
 
-    const target = CommandStorage.getCommand(commandName as string);
+    const target = CommandStorage.getCommand(cmdName as string);
+
     if (target) {
       const command = this.moduleRef.get(target);
 
       if (command) {
-        return command.execute(args, message);
+        command.execute(args, message);
+        return [null];
+      }
+    } else if (cmdName) {
+      const suggestionMessage = generateSuggestionMessage(cmdName as string);
+
+      try {
+        const suggestCommand = CommandStorage.getCommand('suggest');
+        if (suggestCommand) {
+          const command = this.moduleRef.get(suggestCommand);
+          if (command) {
+            command.execute([], message, suggestionMessage);
+          }
+        }
+      } catch (error) {
+        console.error('Error sending suggestion message:', error);
       }
     }
+
+    return [null];
   }
 }
